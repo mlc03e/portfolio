5d3a73c7ad51af935416f23a84b172105839a299
github username <github email address>
2019-06-08 17:56:54 -0400

    video demos added

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect} from "react-router-dom";
 
 class Home extends Component {
+  state= {
+    showPortfolio: false,
+    showAbout: false
+  }
+
+goToPortfolio= () => {
+  // console.log('hi');
+ this.setState({showPortfolio: true})
+}
+goToAbout= () => {
+  // console.log('hi');
+ this.setState({showAbout: true})
+}
 
   render() {
     return (
       <div className='homeOutter'>
-        <div className='homeInner'>
-        <span> Designer </span>
+        <div className='homeInner' onClick={this.goToPortfolio}>
+        {this.state.showPortfolio && <Redirect to="/portfolio" />}
+        <span> designer </span>
         </div>
-        <div className='homeInner'>
-        <span> Coder </span>
+        <div className='homeInner' onClick={this.goToAbout}>
+        {this.state.showAbout && <Redirect to="/about" />}
+        <span>  developer  </span>
         </div>
+
       </div>
     );
   }
--- my-app/src/Portfolio.js
+++ my-app/src/Portfolio.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
+
+import { Player } from 'video-react';
+
 class Portfolio extends Component {
 
   render() {
     return (
-      <div></div>
+      <div>
+      <Player>
+        <source src={require("./ChickenFishSteak.1.mp4")}/>
+      </Player>
+      <Player>
+        <source src={require("./PoV.mov")}/>
+      </Player>
+      </div>
     );
   }
 
