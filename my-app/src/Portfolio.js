3f14d0d9587d203a7d962d56a2cb2ab508ccc223
github username <github email address>
2019-06-16 14:29:43 -0400

    videos fixed

--- my-app/src/Portfolio.js
+++ my-app/src/Portfolio.js
@@ -7,17 +7,25 @@ class Portfolio extends Component {
 
   render() {
     return (
-      <div>
-      <Player>
-        <source src={require("./ChickenFishSteak.1.mp4")}/>
-      </Player>
-      <Player>
-        <source src={require("./PoV.mov")}/>
-      </Player>
+      <div className= 'demoVideo'>
+        <div className="embed-responsive embed-responsive-21by9">
+          <video width="560" height="315" controls  src={require("./ChickenFishSteak.1.mp4")}  autoplay={false} />
+        </div>
+        <div className="embed-responsive embed-responsive-21by9">
+          <video width="560" height="315" controls src={require("./PoV.mov")}  />
+        </div>
       </div>
+
     );
   }
 
 }
-
+// <div>
+// <Player>
+//   <source src={require("./ChickenFishSteak.1.mp4")}/>
+// </Player>
+// <Player>
+//   <source src={require("./PoV.mov")}/>
+// </Player>
+// </div>
 export default Portfolio;
