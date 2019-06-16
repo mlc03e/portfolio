c72b022a3d076a97ab3e11a61d218bb02471e94a
github username <github email address>
2019-06-16 14:44:24 -0400

    demo descriptions

--- my-app/src/Portfolio.js
+++ my-app/src/Portfolio.js
@@ -7,15 +7,31 @@ class Portfolio extends Component {
 
   render() {
     return (
-      <div className= 'demoVideo'>
-        <div className="embed-responsive embed-responsive-21by9">
-          <video width="560" height="315" controls  src={require("./ChickenFishSteak.1.mp4")}  autoplay={false} />
-        </div>
-        <div className="embed-responsive embed-responsive-21by9">
-          <video width="560" height="315" controls src={require("./PoV.mov")}  />
+      <div>
+        <span> Web Development/App Design</span>
+        <div className= 'demoVideo'>
+          <div className="embed-responsive embed-responsive-21by9">
+            <video width="560" height="315" controls  src={require("./ChickenFishSteak.1.mp4")}  autoplay={false} />
+
+          </div>
+
+          <div className="embed-responsive embed-responsive-21by9">
+            <video width="560" height="315" controls src={require("./PoV.mov")}  />
+
+          </div>
+          <div className= 'description'>
+            <span> Chicken Fish or Steak </span>
+            <p> Creates virtual guest books for weddings. Couples can invite guests who can share comments and photos.</p>
+            <p> Rails backend React frontend Cloudinary API</p>
+          </div>
+          <div className= 'description'>
+
+            <span> PoV </span>
+            <p> A platform where clothing stylists can put together their clients’ ideas through a visual moodboard.</p>
+            <p> JSON databse React frontend </p>
+          </div>
         </div>
       </div>
-
     );
   }
 
