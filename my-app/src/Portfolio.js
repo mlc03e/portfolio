85ba418f5b12d6326c576d3b7efa488512b80bc1
github username <github email address>
2019-06-29 15:10:34 -0400

    process component conditionally renderred

--- my-app/src/Portfolio.js
+++ my-app/src/Portfolio.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import ProcessCard from './ProcessCard'
 
 
 // import { Player } from 'video-react';
 
 class Portfolio extends Component {
+
+  state= {
+    processChicken:false,
+    processPov: false
+  }
+  showProcess= (demo) => {
+    if (demo === 'chicken') {
+      this.setState({processChicken: true})
+    }
+    if (demo === 'pov') {
+      this.setState({processPov: true})
+    }
+
+  }
 
   render() {
     return (
@@ -16,22 +31,28 @@ class Portfolio extends Component {
                 <span> Chicken Fish or Steak </span>
                 <p> Creates virtual guest books for weddings. Couples can invite guests who can share comments and photos.</p>
                 <p> Rails backend React frontend Cloudinary API</p>
-                <button>Learn More</button>
+                <button type="button" class="btn btn-outline-dark" onClick={()=>this.showProcess('chicken')}>Learn More</button>
               </div>
               <div className="embed-responsive embed-responsive-21by9">
                 <video width="560" height="315" controls  src={require("./ChickenFishSteak.1.mp4")}  autoplay={false} />
               </div>
+          </div>
+          <div>
+            {this.state.processChicken && <ProcessCard />}
           </div>
           <div className='innerDemo'>
               <div className= 'description'>
                 <span> PoV </span>
                 <p> A platform where clothing stylists can put together their clients’ ideas through a visual moodboard.</p>
                 <p> JSON databse React frontend </p>
-                <button>Learn More</button>
+                <button type="button" class="btn btn-outline-dark" onClick={()=>this.showProcess('pov')}>Learn More</button>
               </div>
               <div className="embed-responsive embed-responsive-21by9">
                 <video width="560" height="315" controls src={require("./PoV.mov")}  />
               </div>
+          </div>
+          <div>
+            {this.state.processPov && <ProcessCard />}
           </div>
         </div>
         <span id='outerSpans'> Graphic Design </span>
---
+++ my-app/src/ProcessCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ProcessCard extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1> ProcessCard </h1>
+      </div>
+    );
+  }
+
+}
+
+export default ProcessCard;
